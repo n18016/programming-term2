01af9989de914c5596a5844144836986f7b75208
n18016 <n18016@std.it-college.ac.jp>
2018-07-10 16:25:45 +0900

    07/05

--- src/basic-c2/task20180705.py
+++ src/basic-c2/task20180705.py
@@ -2,7 +2,7 @@
 # 値段
 beer = 200
 otumami = 100
-yakitori = 80
+yakitori = 100
 # 個数
 beer_n = 2
 otumami_n = 1
@@ -12,6 +12,9 @@ rate = 0.2
 # 割引
 point_off = 150
 # 計算
-goukei = (beer * beer_n) + (otumami * otumami_n) + (yakitori * yakitori_n) - point_off
+beer_sum = beer * beer_n
+otumami_sum = otumami * otumami_n
+yakitori_sum = yakitori * yakitori_n * (1 - rate)
+goukei = beer_sum + otumami_sum + yakitori_sum - point_off
 # 結果を表示
 print("買い物の合計は", goukei, "円" )
