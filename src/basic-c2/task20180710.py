be50aeb3c3cd56a04d7ed7e3d0fcb2a1ca0e4861
n18016 <n18016@std.it-college.ac.jp>
2018-07-13 14:14:27 +0900

    07/11課題

--- src/basic-c2/task20180710.py
+++ src/basic-c2/task20180710.py
@@ -3,16 +3,16 @@ print("じゃんけんをしましょう")
 jan_ken = int(input("何を出すか数字を入力してください。1:グー、2:チョキ、3:パー"))
 
 #変数
-Rock = "CPUがグーを出しました。あなたの負けです。"
+rock = "CPUがグーを出しました。あなたの負けです。"
 scissors = "CPUがチョキを出しました。あなたの負けです。"
 paper = "CPUがパーを出しました。あなたの負けです。"
 error = "入力が不正です。終了します。"
 #判定
 if jan_ken == 1:
-    print(Rock)
+    print(rock.format("パー"))
 elif jan_ken == 2:
-    print(scissors)
+    print(scissors.format("グー"))
 elif jan_ken == 3:
-    print(paper)
+    print(paper.format("チョキ"))
 else:
     print(error)
