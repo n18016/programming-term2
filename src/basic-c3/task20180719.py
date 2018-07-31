252c2eee6d564363a5884bb2a5629774ad726024
n18016 <n18016@std.it-college.ac.jp>
2018-07-31 14:39:30 +0900

    7/19課題

--- src/basic-c3/task20180719.py
+++ src/basic-c3/task20180719.py
@@ -1,26 +1,29 @@
-# 四則計算プログラム
-print("四則計算プログラムです。")
 
 # 足し算を行うための関数を定義
 def add(a, b):
     '''足し算を行う関数'''
     return a + b
+
 # 引き算を行うための関数を定義
 def sub(a, b):
     '''引き算を行う関数'''
     return a - b
+
 # 掛け算を行うための関数を定義
 def mul(a, b):
     '''掛け算を行う関数'''
     return a * b
+
 # 割り算を行うための関数を定義
 def div(a, b):
     '''割り算を行う関数'''
     return a / b
+
 # 四則計算
 print("四則計算のプログラムです。")
 while True:
     parameter1 = input("第１パラメータを入力してください>>>")
+    try:
     num1 = float(parameter1)
     break
     except ValueError:
@@ -28,6 +31,7 @@ while True:
 
 while True:
     parameter2 = input("第２パラメータを入力してください。")
+    try:
     num2 = float(parameter2)
     break
     except ValueError:
@@ -38,18 +42,22 @@ while True:
     if user == "+":
         add = parameter1 + parameter2
         print(add)
+        print(help(add))
         break
-    if user == "-":
+    elif user == "-":
         sub = parameter1 - parameter2
         print(sub)
+        print(help(sub))
         break
-    if user == "*":
+    elif user == "*":
         mul = parameter1 * parameter2
         print(mul)
+        print(help(mul))
         break
-    if user == "/":
+    elif user == "/":
         div = parameter1 / parameter2
         print(div)
+        print(help(div))
         break
     else:
         print("入力が間違っています。")
